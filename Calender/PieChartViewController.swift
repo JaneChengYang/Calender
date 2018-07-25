6ecb4b3c2f80e4edff07ecbd31e69717e7796588
“JaneChengYang” <you@example.com>
2018-07-25 22:55:08 +0800

    add code

--- Calender/PieChartViewController.swift
+++ Calender/PieChartViewController.swift
@@ -12,7 +12,6 @@ class PieChartViewController: UIViewController{
     var diary:DiaryUser?
     var total:String?
     var start:CGFloat = 0
-    //var numberArray = [Double]()
     var foodArray = [Double]()
     var commutingArray = [Double]()
     var entertainmentArray = [Double]()
@@ -46,6 +45,7 @@ class PieChartViewController: UIViewController{
         super.viewDidLoad()
         if let total = total{
             let totals = Double(total)
+            var totalDiarys:Double = 0
         if let diary = diary{
             if let totalDiary = diary.totalUser?.allObjects as? [TotalUser]{
                 var foodTotal = totalDiary.filter { $0.user == "美食" }
@@ -53,14 +53,18 @@ class PieChartViewController: UIViewController{
                 for i in 0 ..< foodTotal.count{
                     let money:Double = Double(foodTotal[i].monay!)!
                     foodArray.append(money)
+                    totalDiarys += Double(foodArray[i])
                     if UIScreen.main.sizeType == .iPhone5{
                         pieChart(radius: 120, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * foodArray[i]) , color: foodColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * foodArray[i])
-                          foodLabel.text = "\(Int(foodArray[i] / totals! * 100))%"
+//                          foodLabel.text = "\(Int(foodArray[i] / totals! * 100))%"
+                        foodLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }else{
                         pieChart(radius: 160, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * foodArray[i]) , color: foodColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * foodArray[i])
-                          foodLabel.text = "\(Int(foodArray[i] / totals! * 100))%"
+                          foodLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                         totalDiarys = 0
                     }
                 }
                 var commutingTotal = totalDiary.filter { $0.user == "通勤" }
@@ -68,14 +72,19 @@ class PieChartViewController: UIViewController{
                 for i in 0 ..< commutingTotal.count{
                     let money:Double = Double(commutingTotal[i].monay!)!
                     commutingArray.append(money)
+                    totalDiarys += Double(commutingArray[i])
+
                     if UIScreen.main.sizeType == .iPhone5{
                         pieChart(radius: 120, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * commutingArray[i]) , color: commutingColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * commutingArray[i])
-                        commutingLabel.text = "\(Int(commutingArray[i] / totals! * 100))%"
+                        //commutingLabel.text = "\(Int(commutingArray[i] / totals! * 100))%"
+                        commutingLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }else{
                         pieChart(radius: 160, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * commutingArray[i]) , color: commutingColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * commutingArray[i])
-                        commutingLabel.text = "\(Int(commutingArray[i] / totals! * 100))%"
+                        commutingLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }
                 }
                 var entertainmentTotal = totalDiary.filter { $0.user == "娛樂" }
@@ -83,14 +92,19 @@ class PieChartViewController: UIViewController{
                 for i in 0 ..< entertainmentTotal.count{
                     let money:Double = Double(entertainmentTotal[i].monay!)!
                     entertainmentArray.append(money)
+                    totalDiarys += Double(entertainmentArray[i])
+
                     if UIScreen.main.sizeType == .iPhone5{
                         pieChart(radius: 120, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * entertainmentArray[i]) , color: entertainmentColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * foodArray[i])
-                        entertainmentLabel.text = "\(Int(entertainmentArray[i] / totals! * 100))%"
+//                        entertainmentLabel.text = "\(Int(entertainmentArray[i] / totals! * 100))%"
+                        entertainmentLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }else{
                         pieChart(radius: 160, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * entertainmentArray[i]) , color: entertainmentColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * entertainmentArray[i])
-                        entertainmentLabel.text = "\(Int(entertainmentArray[i] / totals! * 100))%"
+                        entertainmentLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }
                 }
                 
@@ -99,14 +113,19 @@ class PieChartViewController: UIViewController{
                 for i in 0 ..< phoneTotal.count{
                     let money:Double = Double(phoneTotal[i].monay!)!
                     phoneArray.append(money)
+                    totalDiarys += Double(phoneArray[i])
+
                     if UIScreen.main.sizeType == .iPhone5{
                         pieChart(radius: 120, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * phoneArray[i]) , color: phoneColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * phoneArray[i])
-                        phoneLabel.text = "\(Int(phoneArray[i] / totals! * 100))%"
+//                        phoneLabel.text = "\(Int(phoneArray[i] / totals! * 100))%"
+                        phoneLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }else{
                         pieChart(radius: 160, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * phoneArray[i]) , color: phoneColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * phoneArray[i])
-                        phoneLabel.text = "\(Int(phoneArray[i] / totals! * 100))%"
+                        phoneLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }
                 }
                 
@@ -115,14 +134,19 @@ class PieChartViewController: UIViewController{
                 for i in 0 ..< medicalTotal.count{
                     let money:Double = Double(medicalTotal[i].monay!)!
                     medicalArray.append(money)
+                    totalDiarys += Double(medicalArray[i])
+
                     if UIScreen.main.sizeType == .iPhone5{
                         pieChart(radius: 120, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * medicalArray[i]) , color: medicalColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * medicalArray[i])
-                        medicalLabel.text = "\(Int(medicalArray[i] / totals! * 100))%"
+//                        medicalLabel.text = "\(Int(medicalArray[i] / totals! * 100))%"
+                        medicalLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }else{
                         pieChart(radius: 160, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * medicalArray[i]) , color: medicalColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * medicalArray[i])
-                        medicalLabel.text = "\(Int(medicalArray[i] / totals! * 100))%"
+                        medicalLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }
                 }
                 
@@ -131,14 +155,19 @@ class PieChartViewController: UIViewController{
                 for i in 0 ..< dailySuppliesTotal.count{
                     let money:Double = Double(dailySuppliesTotal[i].monay!)!
                    dailySuppliesArray.append(money)
+                    totalDiarys += Double(dailySuppliesArray[i])
+
                     if UIScreen.main.sizeType == .iPhone5{
                         pieChart(radius: 120, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * dailySuppliesArray[i]) , color: dailySuppliesColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * dailySuppliesArray[i])
-                        dailySuppliesLabel.text = "\(Int(dailySuppliesArray[i] / totals! * 100))%"
+//                        dailySuppliesLabel.text = "\(Int(dailySuppliesArray[i] / totals! * 100))%"
+                        dailySuppliesLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }else{
                         pieChart(radius: 160, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * dailySuppliesArray[i]) , color: dailySuppliesColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * dailySuppliesArray[i])
-                        dailySuppliesLabel.text = "\(Int(dailySuppliesArray[i] / totals! * 100))%"
+                        dailySuppliesLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }
                 }
                 
@@ -147,14 +176,19 @@ class PieChartViewController: UIViewController{
                 for i in 0 ..< drinkTotal.count{
                     let money:Double = Double(drinkTotal[i].monay!)!
                    drinkArray.append(money)
+                    totalDiarys += Double(drinkArray[i])
+
                     if UIScreen.main.sizeType == .iPhone5{
                         pieChart(radius: 120, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * drinkArray[i]) , color: drinkColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * drinkArray[i])
-                        drinkLabel.text = "\(Int(drinkArray[i] / totals! * 100))%"
+//                        drinkLabel.text = "\(Int(drinkArray[i] / totals! * 100))%"
+                        drinkLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }else{
                         pieChart(radius: 160, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * drinkArray[i]) , color: drinkColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * drinkArray[i])
-                        drinkLabel.text = "\(Int(drinkArray[i] / totals! * 100))%"
+                        drinkLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }
                 }
                 
@@ -163,14 +197,19 @@ class PieChartViewController: UIViewController{
                 for i in 0 ..< otherTotal.count{
                     let money:Double = Double(otherTotal[i].monay!)!
                     otherArray.append(money)
+                    totalDiarys += Double(otherArray[i])
+
                     if UIScreen.main.sizeType == .iPhone5{
                         pieChart(radius: 120, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * otherArray[i]) , color: otherColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * otherArray[i])
-                        otherLabel.text = "\(Int(otherArray[i] / totals! * 100))%"
+//                        otherLabel.text = "\(Int(otherArray[i] / totals! * 100))%"
+                        otherLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                     }else{
                         pieChart(radius: 160, startAngle: start, endAngle: start + CGFloat(2 * Double.pi / totals! * otherArray[i]) , color: otherColor.cgColor)
                         start += CGFloat(2 * Double.pi / totals! * otherArray[i])
-                        otherLabel.text = "\(Int(otherArray[i] / totals! * 100))%"
+                        otherLabel.text = "\(Int(totalDiarys / totals! * 100))%"
+                        totalDiarys = 0
                         }
                     }
                 }
@@ -178,18 +217,6 @@ class PieChartViewController: UIViewController{
         }
         // Do any additional setup after loading the view.
     }
-//    func showColorPercent(centerX x:Int,centerY y:Int,color:UIColor,i:Int) {
-//        if let total = total{
-//        var totals = Double(total)
-//        let colorLabel = UILabel()
-//        colorLabel.frame = CGRect(x: 0, y: 0, width: 50, height: 30)
-//        colorLabel.center = CGPoint(x: x, y: y)
-//        colorLabel.backgroundColor = color
-//        colorLabel.textAlignment = NSTextAlignment.center
-//            colorLabel.text = "\(Int(numberArray[i] / totals! * 100))%"
-//        view.addSubview(colorLabel)
-//        }
-//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
